860bafc000c97376c31ff2d2c0b599521c05c57f
Tayl-r <tmoondina@knights.ucf.edu.com>
2023-03-26 04:15:16 -0400

    HI

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter
 import tkinter.messagebox
+import textwrap
 import customtkinter as ctk
+
+
+def button_event():
+    entry_text = link.get()
+    label2.configure(text=entry_text, wraplength = 500)
 
 ctk.set_appearance_mode('System')
 ctk.set_default_color_theme('blue')
@@ -8,7 +14,7 @@ ctk.set_default_color_theme('blue')
 # window
 window = ctk.CTk()
 window.title('Reddit Web-Scraper')
-window.geometry('780x480')
+window.geometry('1000x1000')
 
 # widgets
 label = ctk.CTkLabel(
@@ -21,9 +27,8 @@ label = ctk.CTkLabel(
 label.pack(padx=10, pady=20)
 
 # Entry for the Reddit URL
-url_var = tkinter.StringVar(window,"Summarize a comment here...")
-print(url_var.get())
-link = ctk.CTkEntry(window, width=350, height=40, textvariable=url_var)
+url_var = tkinter.StringVar()
+link = ctk.CTkEntry(window, width=350, height=40, placeholder_text="Type Here")
 link.pack()
 
 # url_var.set("testasdfasfasdf")
@@ -39,8 +44,7 @@ button = ctk.CTkButton(
     corner_radius= 5, 
     font=ctk.CTkFont(size=20, weight="bold"),
     hover = True,
-    command= lambda: print(f'Button was pressed: URL {url_var.get()}')
-    )
+    command= button_event)
 button.pack(padx=10,pady =10)
 
 # Summary textbox header
@@ -51,7 +55,44 @@ summary_header = ctk.CTkLabel(
     text_color= 'white',
     corner_radius= 5, 
     font=ctk.CTkFont(size=20, weight="bold"))
-summary_header.pack(padx=10, pady=50)
+summary_header.pack(padx=10, pady=40)
+"""
+frame = ctk.CTkFrame(window, width=600, height=150)
+frame.pack()
 
+label2 = ctk.CTkLabel(master=frame,  width=600, height=150)
+label2.pack()
+"""
+optionmenu_var = ctk.StringVar(value="option 2")  # set initial value
+
+def optionmenu_callback(choice):
+    print("optionmenu dropdown clicked:", choice)
+
+    if choice == "option 1":
+        frame2 = ctk.CTkFrame(window, width=600, height=150)
+        frame2.pack()
+
+        label3 = ctk.CTkLabel(master=frame,  width=600, height=150)
+        label3.pack()
+    elif choice == "option 2":
+        frame3 = ctk.CTkFrame(window, width=600, height=150)
+        frame3.pack()
+
+        label4 = ctk.CTkLabel(master=frame,  width=600, height=150)
+        label4.pack()
+
+        frame4 = ctk.CTkFrame(window, width=600, height=150)
+        frame4.pack()
+
+        label5 = ctk.CTkLabel(master=frame,  width=600, height=150)
+        label5.pack()
+
+
+
+combobox = ctk.CTkOptionMenu(master=window,
+                                       values=["option 1", "option 2"],
+                                       command=optionmenu_callback,
+                                       variable=optionmenu_var)
+combobox.pack(padx=20, pady=10)
 # run 
 window.mainloop()
